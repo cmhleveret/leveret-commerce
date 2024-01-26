dfac673af65ce261ae7091e40ac349dbb61f02e1
cmhleveret <cmhleveret@gmail.com>
2024-01-26 15:55:20 +0100

    theme provider added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from 'components/layout/navbar';
+import { ThemeProvider } from 'components/theme/theme-provider';
 import { GeistSans } from 'geist/font';
 import { ensureStartsWith } from 'lib/utils';
 import { ReactNode, Suspense } from 'react';
@@ -35,10 +36,17 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
   return (
     <html lang="en" className={GeistSans.variable}>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
-        <Navbar />
-        <Suspense>
-          <main>{children}</main>
-        </Suspense>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          <Suspense>
+            <main>{children}</main>
+          </Suspense>
+        </ThemeProvider>
       </body>
     </html>
   );
